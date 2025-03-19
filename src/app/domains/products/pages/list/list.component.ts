e6c1ff6d97bff6dba5d5756a0adb858fd75aa456
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-03-19 14:08:22 -0500

    se adiciona eslint al proyecto y se hacen las correcciones detectadas segun la guia de estilos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+import { Component, Input, inject, signal, OnInit, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
-import { HeaderComponent } from '@shared/components/header/header.component';
+
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
@@ -11,10 +11,10 @@ import { Category } from '@shared/models/category.model';
 
 @Component({
     selector: 'app-list',
-    imports: [CommonModule, ProductComponent, HeaderComponent, RouterLinkWithHref],
+    imports: [CommonModule, ProductComponent, RouterLinkWithHref],
     templateUrl: './list.component.html'
 })
-export default class ListComponent {
+export default class ListComponent implements OnInit, OnChanges {
 
   products = signal<Product[]>([]);
   categories = signal<Category[]>([]);
@@ -27,7 +27,7 @@ export default class ListComponent {
     this.getCategories();
   }
 
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges() {
     this.getProducts();
   }
 
@@ -42,7 +42,7 @@ export default class ListComponent {
         this.products.set(products);
       },
       error: () => {
-        
+        console.log('Error fetching products');
       }
     })
   }
@@ -54,7 +54,7 @@ export default class ListComponent {
         this.categories.set(data);
       },
       error: () => {
-        
+        console.log('Error fetching categories');
       }
     })
   }
