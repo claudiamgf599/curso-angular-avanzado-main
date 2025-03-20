5ec7d25397e32012efa13549f98e4a52ac4348f6
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-03-20 09:43:39 -0500

    ajustes en configuracion de prettier / eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
-const prettierRules = require('eslint-plugin-prettier/recommended');
+const prettier = require('eslint-config-prettier');
 
 module.exports = tseslint.config(
   {
@@ -12,7 +12,7 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
-      prettierRules,
+      prettier,
     ],
     processor: angular.processInlineTemplates,
     rules: {
@@ -39,7 +39,7 @@ module.exports = tseslint.config(
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
-      prettierRules,
+      prettier,
     ],
     rules: {},
   }
