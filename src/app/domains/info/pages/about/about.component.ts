a29213e37c5713276f97f83a3ce247701dd357b5
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-03-26 11:40:23 -0500

    usando toSignal para convertir Observables a signal

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -6,6 +6,8 @@ import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-about',
@@ -14,13 +16,26 @@ import { FormsModule } from '@angular/forms';
     CounterComponent,
     WaveAudioComponent,
     HighlightDirective,
-    FormsModule
+    FormsModule,
   ],
   templateUrl: './about.component.html',
 })
 export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
+
+  // toSignal para un Observable que tiene valor inicial
+  // usando el requireSync en true indicamos que están sincronizados el observable y el signal
+  obsWithInit$ = new BehaviorSubject<string>('behavior subject initial value');
+  $withInit = toSignal(this.obsWithInit$, {
+    requireSync: true,
+  });
+
+  // toSignal para un Observable que no tiene valor inicial, aquí no se requiere que sea síncrono porque no hay valor inicial
+  obsWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.obsWithoutInit$.pipe(delay(3000)), {
+    initialValue: '-----',
+  });
 
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -30,5 +45,13 @@ export default class AboutComponent {
   changeMessage(event: Event) {
     const input = event.target as HTMLInputElement;
     this.message.set(input.value);
+  }
+
+  emitWithInit() {
+    this.obsWithInit$.next('nuevo valor - emitWithInit');
+  }
+
+  emitWithoutInit() {
+    this.obsWithoutInit$.next('**** - emitWithoutInit');
   }
 }
