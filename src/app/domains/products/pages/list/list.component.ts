ffe3ea5abccdd6e6d2ffa5342ed6ef5605b2a1be
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-03-26 21:20:55 -0500

    uso de toSignal para recuperar todas las categorias

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,37 +1,29 @@
-import {
-  Component,
-  inject,
-  signal,
-  OnInit,
-  OnChanges,
-  input,
-} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnChanges, inject, input, signal } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
 
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
-import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
-import { Category } from '@shared/models/category.model';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
   imports: [CommonModule, ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
 })
-export default class ListComponent implements OnInit, OnChanges {
-  products = signal<Product[]>([]);
-  categories = signal<Category[]>([]);
+export default class ListComponent implements OnChanges {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
   readonly slug = input<string>();
 
-  ngOnInit() {
-    this.getCategories();
-  }
+  $products = signal<Product[]>([]);
+  $categories = toSignal(this.categoryService.getAll(), {
+    initialValue: [],
+  }); // a este sigal no se le puede hacer set
 
   ngOnChanges() {
     this.getProducts();
@@ -44,21 +36,10 @@ export default class ListComponent implements OnInit, OnChanges {
   private getProducts() {
     this.productService.getProducts({ category_slug: this.slug() }).subscribe({
       next: products => {
-        this.products.set(products);
+        this.$products.set(products);
       },
       error: () => {
         console.log('Error fetching products');
-      },
-    });
-  }
-
-  private getCategories() {
-    this.categoryService.getAll().subscribe({
-      next: data => {
-        this.categories.set(data);
-      },
-      error: () => {
-        console.log('Error fetching categories');
       },
     });
   }
