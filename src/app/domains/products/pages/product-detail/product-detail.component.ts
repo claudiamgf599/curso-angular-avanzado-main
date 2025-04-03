49a21f67257dbc26eac6aa52fc44ec9062c83525
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-04-03 09:50:42 -0500

    uso de metatags para implementar el patron open graph y generar previsualizacion al compartir el enlace de detalle de productos

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,13 +1,8 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import {
-  Component,
-  OnInit,
-  inject,
-  input,
-  linkedSignal,
-  signal,
-} from '@angular/core';
-import { Product } from '@shared/models/product.model';
+import { Component, effect, inject, input, linkedSignal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Meta, Title } from '@angular/platform-browser';
+import { environment } from '@env/environment';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
@@ -16,29 +11,61 @@ import { ProductService } from '@shared/services/product.service';
   imports: [CommonModule, NgOptimizedImage],
   templateUrl: './product-detail.component.html',
 })
-export default class ProductDetailComponent implements OnInit {
-  readonly product_slug = input<string>();
+export default class ProductDetailComponent {
+  readonly product_slug = input.required<string>();
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
-  $product = signal<Product | null>(null);
-  $cover = linkedSignal(() => {
-    const product = this.$product(); // cover se recalcula cada vez que se haga un set a product. los computed no permiten set
-    if (product && product.images.length > 0) {
-      return product.images[0];
-    }
-    return '';
+  productRs = rxResource({
+    request: () => ({
+      slug: this.product_slug(),
+    }),
+    loader: ({ request }) => {
+      return this.productService.getOne({ product_slug: request.slug });
+    },
   });
 
-  ngOnInit() {
-    const product_slug = this.product_slug();
-    if (product_slug) {
-      this.productService.getOne({ product_slug: product_slug }).subscribe({
-        next: product => {
-          this.$product.set(product);
-        },
-      });
-    }
+  $cover = linkedSignal({
+    source: this.productRs.value, // cover se recalcula cada vez que se haga un set a product. los computed no permiten set
+    computation: (product, previousValue) => {
+      if (product && product.images.length > 0) {
+        return product.images[0];
+      }
+      return previousValue?.value;
+    },
+  });
+
+  titleService = inject(Title);
+  metaService = inject(Meta);
+
+  constructor() {
+    effect(() => {
+      // esto se hace para seguir el Open Graph protocol y lograr previsualizaciones al compartir el enlace
+      const product = this.productRs.value();
+      if (product) {
+        this.titleService.setTitle(product.title);
+        this.metaService.updateTag({
+          name: 'description',
+          content: product.description,
+        });
+        this.metaService.updateTag({
+          property: 'og:title',
+          content: product.title,
+        });
+        this.metaService.updateTag({
+          property: 'og:description',
+          content: product.description,
+        });
+        this.metaService.updateTag({
+          property: 'og:image',
+          content: product.images[0],
+        });
+        this.metaService.updateTag({
+          property: 'og:url',
+          content: `${environment.domain}/product/${product.slug}`,
+        });
+      }
+    });
   }
 
   changeCover(newImg: string) {
@@ -46,7 +73,7 @@ export default class ProductDetailComponent implements OnInit {
   }
 
   addToCart() {
-    const product = this.$product();
+    const product = this.productRs.value();
     if (product) {
       this.cartService.addToCart(product);
     }
