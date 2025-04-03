40c1f4fa98e47a769b47f3dbff4a45fbe57070b0
CEIBA\claudia.gomez <claudia.gomez@ceiba.com.co>
2025-04-03 10:36:18 -0500

    implementacion de un service para agregar los metatags, por ahora se usa desde detalle de productos

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, effect, inject, input, linkedSignal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { Meta, Title } from '@angular/platform-browser';
 import { environment } from '@env/environment';
 import { CartService } from '@shared/services/cart.service';
+import { MetaTagsService } from '@shared/services/meta-tags.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
@@ -15,6 +15,7 @@ export default class ProductDetailComponent {
   readonly product_slug = input.required<string>();
   private productService = inject(ProductService);
   private cartService = inject(CartService);
+  private metaTagsService = inject(MetaTagsService);
 
   productRs = rxResource({
     request: () => ({
@@ -35,34 +36,16 @@ export default class ProductDetailComponent {
     },
   });
 
-  titleService = inject(Title);
-  metaService = inject(Meta);
-
   constructor() {
     effect(() => {
       // esto se hace para seguir el Open Graph protocol y lograr previsualizaciones al compartir el enlace
       const product = this.productRs.value();
       if (product) {
-        this.titleService.setTitle(product.title);
-        this.metaService.updateTag({
-          name: 'description',
-          content: product.description,
-        });
-        this.metaService.updateTag({
-          property: 'og:title',
-          content: product.title,
-        });
-        this.metaService.updateTag({
-          property: 'og:description',
-          content: product.description,
-        });
-        this.metaService.updateTag({
-          property: 'og:image',
-          content: product.images[0],
-        });
-        this.metaService.updateTag({
-          property: 'og:url',
-          content: `${environment.domain}/product/${product.slug}`,
+        this.metaTagsService.updateMetaTags({
+          title: product.title,
+          description: product.description,
+          image: product.images[0],
+          url: `${environment.domain}/product/${product.slug}`,
         });
       }
     });
